0786bf3d2fe5897e10655436b58721ba742562f5
Amiran <amiranlevanovich@gmail.com>
2020-09-22 16:18:45 +0300

    Added devise gem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+gem 'devise'
 gem 'puma', '~> 4.1'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
 gem 'twitter-bootstrap-rails'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
   # Задаем объект @event для тех действий, где он нужен
-  before_action :set_event, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[show index]
+  before_action :set_event, only: %i[show]
+  before_action :set_current_user_event, only: %i[edit update destroy]
 
   def index
     @events = Event.all
@@ -9,16 +11,16 @@ class EventsController < ApplicationController
   def show; end
 
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   def edit; end
 
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
+      redirect_to @event, notice: I18n.t('controllers.events.created')
     else
       render :new
     end
@@ -26,7 +28,7 @@ class EventsController < ApplicationController
 
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: 'Event was successfully updated.'
+      redirect_to @event, notice: I18n.t('controllers.events.updated')
     else
       render :edit
     end
@@ -34,16 +36,19 @@ class EventsController < ApplicationController
 
   def destroy
     @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
+    redirect_to events_url, notice: I18n.t('controllers.events.destroyed')
   end
 
   private
+
+  def set_current_user_event
+    @event = current_user.events.find(params[:id])
+  end
 
   def set_event
     @event = Event.find(params[:id])
   end
 
-  # редактируем параметры события
   def event_params
     params.require(:event).permit(:title, :address, :datetime, :description)
   end
