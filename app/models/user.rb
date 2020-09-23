b1b6744b54a4ecfa2c39f0fd96adb66522e28773
Amiran <amiranlevanovich@gmail.com>
2020-09-23 13:25:21 +0300

    Changed user name on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   private
 
   def set_name
-    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+    self.name = "Уважаемый человек №#{rand(777)}" if self.name.blank?
   end
 end
