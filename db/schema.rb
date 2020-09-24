637beb01d5e701ea3e573d9e46ae0bcd7f8e22c4
Amiran <amiranlevanovich@gmail.com>
2020-09-24 16:15:35 +0300

    moved delete event button, clear the mess with db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_130405) do
+ActiveRecord::Schema.define(version: 2020_09_24_101505) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
