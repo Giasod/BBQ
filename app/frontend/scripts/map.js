adbe0b3ffc04feb1dfffaa108554b8eeb0649349
Amiran <amiranlevanovich@gmail.com>
2020-10-02 22:10:38 +0300

    fixed map.js & trying to fix no photo upload, currently working but not ideal

--- app/frontend/scripts/map.js
+++ app/frontend/scripts/map.js
@@ -2,31 +2,37 @@ ymaps.ready(init);
 var myMap;
 
 function init(){
-    address = document.getElementById('map').getAttribute('data-address');
+    element_id = document.getElementById('map')
 
-    myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
 
-    myGeocoder = ymaps.geocode(address);
+    if (element_id != null){
 
-    myGeocoder.then(
-        function (res) {
-            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+        address = element_id.getAttribute('data-address');
 
-            myMap.geoObjects.add(
-                new ymaps.Placemark(
-                    coordinates,
-                    {iconContent: address},
-                    {preset: 'islands#blueStretchyIcon'}
-                )
-            );
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 10
+        });
 
-            myMap.setCenter(coordinates);
-            myMap.setZoom(15);
-        }, function (err) {
-            alert('Ошибка при определении местоположения');
-        }
-    );
+        myGeocoder = ymaps.geocode(address);
+
+        myGeocoder.then(
+            function (res) {
+                coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+
+                myMap.geoObjects.add(
+                    new ymaps.Placemark(
+                        coordinates,
+                        {iconContent: address},
+                        {preset: 'islands#blueStretchyIcon'}
+                    )
+                );
+
+                myMap.setCenter(coordinates);
+                myMap.setZoom(15);
+            }, function (err) {
+                alert('Ошибка при определении местоположения');
+            }
+        );
+    }
 }
