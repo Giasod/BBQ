e30792376579441397674eecd7dd76a126d11054
Amiran <amiranlevanovich@gmail.com>
2020-10-19 14:21:02 +0300

    still sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
       :port           => '587',
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :password       => ENV['SENDGRID_API_KEY'],
       :domain         => ENV['SENDGRID_DOMAIN'],
       :enable_starttls_auto => true
   }
