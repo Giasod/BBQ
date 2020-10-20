5af1bcc282ec39930659ee79f5dc748271b2166e
Amiran <amiranlevanovich@gmail.com>
2020-10-20 20:54:27 +0300

    still trying to fix aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ if Rails.env.production?
         region: ENV['S3_BUCKET_REGION'],
         aws_access_key_id: ENV['S3_ACCESS_KEY'],
         aws_secret_access_key: ENV['S3_SECRET_KEY'],
-        :endpoint => 'http://s3.amazonaws.com'
+        :path_style => true
     }
     
     config.fog_directory = ENV['S3_BUCKET_NAME']
