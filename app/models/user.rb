fa48fea307142852b223dd20289c190409eb3146
Amiran <amiranlevanovich@gmail.com>
2020-10-26 15:47:27 +0300

    username uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,33 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
-
+  
   validates :name, presence: true,
-                   length: { maximum: 35 }
+            uniqueness: true,
+            length: { maximum: 35 }
+  
   validates :email, presence: true
-
+  
   before_validation :set_name, on: :create
   after_commit :link_subscriptions, on: :create
   
   mount_uploader :avatar, AvatarUploader
   
   private
-
+  
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: self.email)
         .update_all(user_id: self.id)
   end
-
+  
   def set_name
     self.name = "Уважаемый человек №#{rand(777)}" if self.name.blank?
   end
-
+  
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
