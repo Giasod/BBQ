cd4ca2384f942254f3830f9b8c73cfa0e952384c
Amiran <amiranlevanovich@gmail.com>
2020-11-18 20:26:34 +0300

    added user_pick to oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,39 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   
   private
+
+  def self.find_for_oauth(access_token)
+    # Достаём email из токена
+    email = access_token.info.email
+    user = where(email: email).first
+    
+  
+    # Возвращаем, если нашёлся
+    return user if user.present?
+  
+    # Если не нашёлся, достаём провайдера, айдишник и урл
+    provider = access_token.provider
+    id = access_token.extra.raw_info.id
+  
+    case provider
+    when 'facebook'
+      url = "https://facebook.com/#{id}"
+      avatar = access_token.info.image.gsub('http', 'https')
+    when 'vkontakte'
+      url = "https://vk.com/#{id}"
+      avatar = access_token.info.image.gsub('http', 'https')
+    end
+  
+  
+    # Теперь ищем в базе запись по провайдеру и урлу
+    # Если есть, то вернётся, если нет, то будет создана новая
+    where(url: url, provider: provider).first_or_create! do |user|
+      # Если создаём новую запись, прописываем email и пароль
+      user.email = email
+      user.remote_avatar_url = avatar
+      user.password = Devise.friendly_token.first(16)
+    end
+  end
   
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: self.email)
@@ -32,34 +65,4 @@ class User < ApplicationRecord
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
-
-  def self.find_for_oauth(access_token)
-    # Достаём email из токена
-    email = access_token.info.email
-    user = where(email: email).first
-  
-    # Возвращаем, если нашёлся
-    return user if user.present?
-  
-    # Если не нашёлся, достаём провайдера, айдишник и урл
-    provider = access_token.provider
-    id = access_token.extra.raw_info.id
-    
-    case provider
-    when 'facebook'
-      url = "https://facebook.com/#{id}"
-    when 'vkontakte'
-      url = "https://vk.com/#{id}"
-    end
-    
-  
-    # Теперь ищем в базе запись по провайдеру и урлу
-    # Если есть, то вернётся, если нет, то будет создана новая
-    where(url: url, provider: provider).first_or_create! do |user|
-      # Если создаём новую запись, прописываем email и пароль
-      user.email = email
-      user.password = Devise.friendly_token.first(16)
-    end
-  end
-
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,10 +273,11 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, Rails.application.secrets.omniauth_facebook_id,
-                  Rails.application.secrets.omniauth_facebook_secret
+                  Rails.application.secrets.omniauth_facebook_secret,
+                  image_size: 'large'
   config.omniauth :vkontakte, Rails.application.secrets.omniauth_vkontakte_id,
                   Rails.application.secrets.omniauth_vkontakte_secret,
-                  scope: 'email'
+                  scope: 'email', image_size: 'bigger_x2'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
